8e8e264418dd8cdfb9dd15aeac9e312d2845d82c
MirenaBorisova <mirena.angelova@gmail.com>
2018-07-17 15:39:04 +0300

    new repo

---
+++ Angular-Tour-Of-Heroes/angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+// import { Hero } from './hero';
+
+// export const HEROES: Hero[] = [
+//     { id: 1, name: 'Mr. Nice' },
+//     { id: 2, name: 'Mr. Strong' },
+//     { id: 3, name: 'Mr. Perfect' },
+//     { id: 4, name: 'Miss Charming' },
+//     { id: 5, name: 'Miss Beauty' },
+//     { id: 6, name: 'Miss Blondy' },
+//     { id: 7, name: 'Miss Barby' },
+//     { id: 8, name: 'Mr Muscle' },
+//     { id: 9, name: 'Mr Brain' },
+//     { id: 10, name: 'Miss Smart' }
+// ];
